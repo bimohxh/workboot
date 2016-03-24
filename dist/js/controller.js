4ab7b1fe40ab1cdf70155b67fea7f08ed344883f
黄小红 <hxh@localhost.(none)>
2016-03-24 02:40:34 -0700

    add sass

--- dist/js/controller.js
+++ dist/js/controller.js
@@ -49,13 +49,53 @@
 	var _Game = __webpack_require__(1);
 
 	var game = new _Game.Game();
-	alert(game.play());
+
+	/*var promises = [2,3,5,7,11].map(function(id){
+	  return new Promise(function(resolve, reject){
+	    var timeout = 1000
+	    if (id === 2) {timeout = 3000}
+	    setTimeout(function(){
+	      console.log(id)
+	      resolve()
+	    },timeout)
+	  })
+	})*/
+
+	var getP = function getP(i) {
+	  return new Promise(function (resolve, reject) {
+	    var timeout = 1000;
+	    if (i === 2) {
+	      timeout = 3000;
+	    }
+	    setTimeout(function () {
+	      console.log(i);
+	      resolve();
+	    }, timeout);
+	  });
+	};
+
+	Promise.resolve().then(function () {
+	  return getP(2);
+	}).then(function () {
+	  return getP(3);
+	}).then(function () {
+	  return getP(4);
+	}).then(function () {
+	  return getP(5);
+	}).then(function () {
+	  return getP(6);
+	});
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
 	'use strict';
+
+	/**
+	 * 比赛模块
+	 * @class
+	 */
 
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
@@ -64,11 +104,6 @@
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	/**
-	 * 比赛模块
-	 * @class
-	 */
 
 	var Game = function () {
 
@@ -107,6 +142,19 @@
 	    key: 'play',
 	    value: function play() {
 	      return "开始比赛了";
+	    }
+
+	    /**
+	     * 计算两个数字的乘积
+	     * @param  {number} num1 第一个数字
+	     * @param  {number} num2 第二个数字
+	     * @return {number}      计算结果
+	     */
+
+	  }, {
+	    key: 'study',
+	    value: function study(num1, num2) {
+	      return num1 * num2;
 	    }
 	  }]);
 
